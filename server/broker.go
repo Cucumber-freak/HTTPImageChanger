679aac7f570e4d74dbcf62e4ff6101786a53b061
4opix <idanilov612@gmail.com>
2026-02-17 19:50:12 +0300

    feat
    added connection to RabbitMQ on broker file

--- server/broker.go
+++ server/broker.go
@@ -1,5 +1,47 @@
 package server
 
-type RbClient struct {
-	conn string
+import (
+	"context"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitClient struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+	Queue   string
+}
+
+func ConnectRabbit(url, queueName string) *RabbitClient {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		panic(fmt.Sprintf("Ошибка RabbitMQ: %v", err))
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = ch.QueueDeclare(queueName, true, false, false, false, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &RabbitClient{Conn: conn, Channel: ch, Queue: queueName}
+}
+
+func (r *RabbitClient) Publish(taskID string) error {
+	return r.Channel.PublishWithContext(
+		context.Background(),
+		"",
+		r.Queue,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(taskID),
+		},
+	)
 }
