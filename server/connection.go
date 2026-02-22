5da0e20a81bc18c0f7ea4df8f59908e724d38f49
4opix <idanilov612@gmail.com>
2026-02-23 00:57:14 +0300

    feat: fixed uploads and saves errors

--- server/connection.go
+++ server/connection.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"context"
 	"io"
 	"log"
 	"net/http"
@@ -41,12 +40,28 @@ func (s *Server) UploadHandler(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	taskId := uuid.New().String()
-	var ctx context.Context
-	s.DB.SaveImg(ctx, taskId, header.Filename)
+	ctx := r.Context()
+
+	err = s.S3.Upload(ctx, taskId, file, header.Size)
+	if err != nil {
+		http.Error(w, "S3 error", http.StatusInternalServerError)
+		return
+	}
+
+	err = s.DB.SaveImg(ctx, taskId, header.Filename)
 	if err != nil {
 		http.Error(w, "Db error", http.StatusInternalServerError)
 		return
 	}
+
+	err = s.Broker.Publish(ctx, taskId)
+	if err != nil {
+		http.Error(w, "RabbitMQ error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(taskId))
 }
 
 func (s *Server) DownloadHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,12 +70,13 @@ func (s *Server) DownloadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	imageID := chi.URLParam(r, "id")
+	ctx := r.Context()
 	if imageID == "" {
 		http.Error(w, "Missing ID", http.StatusBadRequest)
 		return
 	}
 	compressedName := imageID + "_compressed"
-	reader, err := s.S3.Download(compressedName)
+	reader, err := s.S3.Download(ctx, compressedName)
 	if err != nil {
 		log.Printf("File not found in S3: %v", err)
 		http.Error(w, "Image not ready or not found", http.StatusNotFound)
